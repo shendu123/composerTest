524f9b7537e1fc72432f5bdf2251ee03b393bfaa
unknown <764055773@qq.com>
2018-08-03 23:01:58 +0800

    composer package

---
+++ src/Demo/FirstPackage/Tools.php
@@ -0,0 +1,15 @@
+<?php
+namespace Demo\FirstPackage;
+
+class Tools{
+
+    public function __construct()
+    {
+
+    }
+
+    public static function Hello(){
+        echo "hello world";
+    }
+}
+?>
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+    include './vendor/autoload.php';
+    use Demo\FirstPackage\Tools;
+
+    Tools::Hello();
+?>
